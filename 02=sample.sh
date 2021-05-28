b03169c8afc57a73a8b4e8691d9a826c7d62ea22
sainath <kalvasainathreddy@gmail.com>
2021-05-28 14:38:47 +0530

    pragraph printing and using tab space using \t

--- 02=sample.sh
+++ 02=sample.sh
@@ -7,3 +7,10 @@ echo hello world
 # echo to print multiple lines
 # syntax: echo -e "line1/n line2"
  echo -e "hello,\n\nwelcome to devops training"
+
+ # paragraph printing and using \t for tab space
+
+ echo -e "\tThe extension is needed as people are increasingly roaming in public,
+            especially in Tokyo and Osaka, when they should be doing the opposite,
+            and it’s feared that lifting the emergency would cause an instant rebound in infections,
+            Economy Revitalization Minister Yasutoshi Nishimura said."
