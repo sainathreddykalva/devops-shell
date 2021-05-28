727aeedda8f0e515add239fc1d00bc6b1600a9cb
sainath <kalvasainathreddy@gmail.com>
2021-05-28 14:17:52 +0530

    printing multiple lines on with one echo

--- 02=sample.sh
+++ 02=sample.sh
@@ -3,3 +3,7 @@
 #echo and print are the command to print something on the screen
 
 echo hello world
+
+# echo to print multiple lines
+# syntax: echo -e "line1/n line2"
+ echo -e "hello,/nwelcome to devops training"
