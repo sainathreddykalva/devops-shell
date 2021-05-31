1bff2183cfa2c7bc1aea590d4e75b0eb22f71a13
sainath <kalvasainathreddy@gmail.com>
2021-05-31 23:02:34 +0530

    return with err code

--- 07-functions.sh
+++ 07-functions.sh
@@ -5,7 +5,7 @@
 sample() {
   echo welcome to sample function
   ## come out of function using return comand
-  return
+  return 7
   echo value of a =$a
   echo first argument =$1
   echo second argument =$2
@@ -20,5 +20,5 @@ sample 300 500
 
 ## taking cli arguments wit functions
 sample $1 $2
-
+echo exit status of smaple function = $?
 echo value of b =$b
