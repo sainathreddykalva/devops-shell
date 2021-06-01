8277b45bebf3f341b6bbb0d8f314ae6416845c5d
sainath <kalvasainathreddy@gmail.com>
2021-06-01 21:22:31 +0530

    fully automated script of nginx

--- roboshop/coponents/frontend.sh
+++ roboshop/coponents/frontend.sh
@@ -7,6 +7,23 @@ HEAD "Installing Nginx "
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
+HEAD "Download From Github\t"
+curl -s -L -o /tmp/frontend.zip "https://github.com/roboshop-devops-project/frontend/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Delete Old HTML Docs\t"
+rm -rf /usr/share/nginx/html/*
+STAT $?
+
+HEAD "Extract Downloaded content"
+unzip -d /usr/share/nginx/html /tmp/frontend.zip &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/frontend-main/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+mv /usr/share/nginx/html/static/* /usr/share/nginx/html/. &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Update Nginx Configuration"
+mv /usr/share/nginx/html/localhost.conf /etc/nginx/default.d/roboshop.conf
+STAT $?
 HEAD "Start Nginx\t"
 systemctl start nginx &>>/tmp/roboshop.log
 systemctl enable nginx &>>/tmp/roboshop.log
