7a2a10e36f1842e4b63ccc8b3d1270f2f3909434
sainath <kalvasainathreddy@gmail.com>
2021-06-01 19:52:42 +0530

    front end uses head fun

--- roboshop/coponents/common.sh
+++ roboshop/coponents/common.sh
@@ -5,7 +5,7 @@ HEAD() {
    if [ $1 -eq 0]; then
      echo -e "\e[1;32m done\e[0m"
      else
-       echo -e "\e[1;33m fail\e[0m"
+       echo -e "\e[1;32m fail\e[0m"
        echo -e "\e[1;33m check the logfile more details  ...log file: /tmp/roboshop.log\e[0m"
        exit 1
        fi
