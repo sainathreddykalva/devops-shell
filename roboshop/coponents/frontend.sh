6fdbc8a490d046e54f2e5a18fd41231b80274225
sainath <kalvasainathreddy@gmail.com>
2021-06-01 21:14:36 +0530

    starting nginx servie

--- roboshop/coponents/frontend.sh
+++ roboshop/coponents/frontend.sh
@@ -7,7 +7,7 @@ HEAD "Installing Nginx "
 yum install nginx -y &>>/tmp/roboshop.log
 STAT $?
 
-#head "Start Nginx\t"
-#systemctl start nginx &>>/tmp/roboshop.log
-#systemctl enable nginx &>>/tmp/roboshop.log
-#STAT $?
+HEAD "Start Nginx\t"
+systemctl start nginx &>>/tmp/roboshop.log
+systemctl enable nginx &>>/tmp/roboshop.log
+STAT $?
