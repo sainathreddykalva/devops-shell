96254b90adf971bf7bcecc8998c62d606ced7507
sainath <kalvasainathreddy@gmail.com>
2021-06-02 19:02:54 +0530

    launch.sh first creation

---
+++ roboshop/launch.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+COMPONENT=$1
+
+## -z validates the variable empty,true if it is empty
+if [ -z "${COMPONENT}" ]; then
+  echo "Component Input is Needed"
+  exit 1
+  fi
+  LID=lt-0b557ee178484346e
+  LVER=1
+
+aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specification  "ResourceType=instance,Tags=[{Key=Name,Value=$COMPONENT}]" | jq
