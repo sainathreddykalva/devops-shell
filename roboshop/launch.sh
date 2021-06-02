81048cd875c3407d5ba891406fd51ba5e0a5c2cb
sainath <kalvasainathreddy@gmail.com>
2021-06-02 20:55:28 +0530

    launch.sh final with no duplicate accept

--- roboshop/launch.sh
+++ roboshop/launch.sh
@@ -9,5 +9,14 @@ if [ -z "${COMPONENT}" ]; then
   fi
   LID=lt-0b557ee178484346e
   LVER=1
+  ##validate if instance is already exists
+ INSTANCE_STAT=$(aws ec2 describe-instances --filters "Name=tag:Name,Values=$COMPONENT" | jq .Reservations[].Instances[].State.Name | xargs -n1)
 
-aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specification  "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
+if [ "$(INSTANCE_STAT)" = "running" ]; then
+  echo "instance already exists!!"
+  exit 0
+  fi
+  if [ "$(INSTANCE_STAT)" = "stopped" ]; then
+    exit 0
+    fi
+    aws ec2 run-instances --launch-template LaunchTemplateId=${LID},Version=${LVER} --tag-specification  "ResourceType=instance,Tags=[{Key=Name,Value=${COMPONENT}}]" | jq
