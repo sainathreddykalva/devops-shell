75e279e326c20723b63385ddec7c0422b7551c99
sainath <kalvasainathreddy@gmail.com>
2021-06-03 21:32:14 +0530

    catalogue

--- roboshop/components/catalogue.sh
+++ roboshop/components/catalogue.sh
@@ -1,3 +1,24 @@
 #!/bin/bash
 
-echo installing catalogue
+source components/common.sh
+rm -f /tmp/roboshop.log
+
+HEAD "Install NodeJS"
+yum install nodejs make gcc-c++ -y &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Add Roboshop App User"
+useradd roboshop &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Download App from GitHub"
+curl -s -L -o /tmp/catalogue.zip "https://github.com/roboshop-devops-project/catalogue/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract the Downloaded Archive"
+cd /home/roboshop && unzip -o /tmp/catalogue.zip &>>/tmp/roboshop.log && mv catalogue-main catalogue
+STAT $?
+
+HEAD "Install NodeJS Dependencies"
+cd /home/roboshop/catalogue && npm install &>>/tmp/roboshop.log
+STAT $?
