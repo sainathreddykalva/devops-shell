5526039848f60d5f5b21c4ff1678bd619c1c9b50
sainath <kalvasainathreddy@gmail.com>
2021-06-05 22:02:18 +0530

    final mysql.sh

--- roboshop/components/mysql.sh
+++ roboshop/components/mysql.sh
@@ -31,7 +31,14 @@ if [ $? -ne 0 ]; then
   STAT $?
 fi
 
-DOWNLOAD_FROM_GITHUB mysql
+HEAD "Download Schema from GitHub\t"
+curl -s -L -o /tmp/mysql.zip "https://github.com/roboshop-devops-project/mysql/archive/main.zip" &>>/tmp/roboshop.log
+STAT $?
+
+HEAD "Extract Downloaded Archive\t"
+cd /tmp
+unzip -o mysql.zip &>>/tmp/roboshop.log
+STAT $?
 
 HEAD "Load Shipping Schema"
 cd /tmp && unzip -o mysql.zip &>>/tmp/roboshop.log && cd mysql-main && mysql -u root -pRoboShop@1 <shipping.sql  &>>/tmp/roboshop.log
